28de79a36d6a261af73cdb4f28e1cfcf64a8cd13
Alexandr Koshelev <oit@uf-vsuwt.ru>
2024-01-02 19:55:22 +0500

    feat: подготовка к деплою

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,8 +81,11 @@ define(['./workbox-9637eeee'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
+    "url": "registerSW.js",
+    "revision": "3ca0b8505b4bec776b69afdba2768812"
+  }, {
     "url": "index.html",
-    "revision": "0.9va0b0050s"
+    "revision": "0.rlq2d5t4b18"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
