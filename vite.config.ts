3d84923390c20a8e9fbc2ceb3a83fd838feca51c
Alexandr Koshelev <oit@uf-vsuwt.ru>
2024-01-03 12:00:09 +0500

    feat: настроил pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
@@ -10,7 +10,6 @@ export default defineConfig({
         enabled: true,
         type: "module",
       },
-
       registerType: "prompt",
       includeAssets: ["**/*"],
       manifest: {
@@ -24,56 +23,56 @@ export default defineConfig({
         orientation: "portrait-primary",
         icons: [
           {
-            src: "./public/icons/icon-48x48.png",
+            src: "icons/icon-48x48.png",
             type: "image/png",
             sizes: "48x48",
             purpose: "any maskable",
           },
           {
-            src: "./public/icons/icon-72x72.png",
+            src: "icons/icon-72x72.png",
             type: "image/png",
             sizes: "72x72",
 
             purpose: "any maskable",
           },
           {
-            src: "./public/icons/icon-96x96.png",
+            src: "icons/icon-96x96.png",
             type: "image/png",
             sizes: "96x96",
             purpose: "any maskable",
           },
           {
-            src: "./public/icons/icon-128x128.png",
+            src: "icons/icon-128x128.png",
             type: "image/png",
             sizes: "128x128",
             purpose: "any maskable",
           },
           {
-            src: "./public/icons/icon-144x144.png",
+            src: "icons/icon-144x144.png",
             type: "image/png",
             sizes: "144x144",
             purpose: "any maskable",
           },
           {
-            src: "./public/icons/icon-152x152.png",
+            src: "icons/icon-152x152.png",
             type: "image/png",
             sizes: "152x152",
             purpose: "any maskable",
           },
           {
-            src: "./public/icons/icon-192x192.png",
+            src: "icons/icon-192x192.png",
             type: "image/png",
             sizes: "192x192",
             purpose: "any maskable",
           },
           {
-            src: "./public/icons/icon-384x384.png",
+            src: "icons/icon-384x384.png",
             type: "image/png",
             sizes: "384x384",
             purpose: "any maskable",
           },
           {
-            src: "./public/icons/icon-512x512.png",
+            src: "icons/icon-512x512.png",
             type: "image/png",
             sizes: "512x512",
             purpose: "any maskable",
